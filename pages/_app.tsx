eea6a080a8ead3bcc5f4224a68b3da97db3a4633
dalbeee <urban.slow.net@gmail.com>
2021-03-22 18:08:18 +0900

    Revert "remove not use code"
    This reverts commit 25a01aad189273f396dd181c03066aa25f6539d3.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
+import { DragDropContext } from "react-beautiful-dnd";
 import Header from "../components/Header";
 import { StoreProvider } from "../hooks/store";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StoreProvider>
-      <div className="flex justify-center h-auto min-h-screen bg-gray-100">
-        <div className="w-11/12 2xl:w-3/5">
-          <Header />
-          <Component {...pageProps} />
+    <DragDropContext>
+      <StoreProvider>
+        <div className="flex justify-center h-auto min-h-screen bg-gray-100">
+          <div className="w-11/12 2xl:w-3/5">
+            <Header />
+            <Component {...pageProps} />
+          </div>
         </div>
-      </div>
-    </StoreProvider>
+      </StoreProvider>
+    </DragDropContext>
   );
 }
 
