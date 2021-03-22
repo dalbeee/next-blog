25a01aad189273f396dd181c03066aa25f6539d3
dalbeee <urban.slow.net@gmail.com>
2021-03-22 16:33:00 +0900

    remove not use code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,17 @@
-import { DragDropContext } from "react-beautiful-dnd";
 import Header from "../components/Header";
 import { StoreProvider } from "../hooks/store";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <DragDropContext>
-      <StoreProvider>
-        <div className="flex justify-center h-auto min-h-screen bg-gray-100">
-          <div className="w-11/12 2xl:w-3/5">
-            <Header />
-            <Component {...pageProps} />
-          </div>
+    <StoreProvider>
+      <div className="flex justify-center h-auto min-h-screen bg-gray-100">
+        <div className="w-11/12 2xl:w-3/5">
+          <Header />
+          <Component {...pageProps} />
         </div>
-      </StoreProvider>
-    </DragDropContext>
+      </div>
+    </StoreProvider>
   );
 }
 
