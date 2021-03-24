7e8b9cb087070b63fdb3334b9e992c103ce188f8
dalbeee <urban.slow.net@gmail.com>
2021-03-24 20:10:53 +0900

    modify: fallback with router

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
 import { IPost } from "../..";
 import { getPost, getPosts } from "../../util/axios";
@@ -12,7 +13,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts: IPost[] = await getPosts();
   const paths = posts.map((post) => ({ params: { slug: post.slug } }));
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 };
 
 const renderers = {
@@ -24,6 +25,10 @@ const renderers = {
 };
 
 const PostDetail = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) return <div>loading...</div>;
+
   return (
     <div className="flex justify-center">
       <div className="w-full" style={{ maxWidth: "860px" }}>
