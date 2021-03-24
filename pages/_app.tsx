e0287a4266031af168b4a0016d67224cf846a68b
dalbeee <urban.slow.net@gmail.com>
2021-03-24 15:36:18 +0900

    ✔ add: title, favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
+import Head from "next/head";
 import Header from "../components/Header";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="flex justify-center h-auto min-h-screen bg-gray-100">
-      <div className="w-11/12 2xl:w-3/5">
-        <Header />
-        <Component {...pageProps} />
+    <>
+      <Head>
+        <title>coding palette</title>
+      </Head>
+      <div className="flex justify-center h-auto min-h-screen bg-gray-100">
+        <div className="w-11/12 2xl:w-3/5">
+          <Header />
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
